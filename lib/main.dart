4f4e4a1a108751774e86a90caea1ca0bd9842d47
babynolow@gmail.com <babynolow@gmail.com>
2024-09-27 19:40:37 +0700

    init project

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 128, 133, 232),
+      appBar: AppBar(
+        backgroundColor:const Color.fromARGB(255, 17, 17, 51) ,
+    title: Text('I am rich'),
+    centerTitle: true,
+  ),
+  body: Center(
+    child: Image.asset('assets/kimcuong.png'),
+  ),
+    );    
+}
+}
